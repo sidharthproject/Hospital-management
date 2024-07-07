7c4d5ad57ea3eb03ec527f3e6fcd344023bf80a8
sidharthproject <sidharthpatro001@gmail.com>
2024-07-07 14:15:08 +0530

    fifty one

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,22 @@
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import { Toaster } from 'react-hot-toast';
 
-
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import { Toaster } from 'react-hot-toast'
 function App() {
- 
-
   return (
     <>
-    <Header/>
-    <Outlet/>
-    <Footer/>
-    <div>
-      <Toaster position='top-right'/>
-    </div>
+      <Header />
+      <Outlet />
+      <Footer />
+      <div>
+        <Toaster position="top-right" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
