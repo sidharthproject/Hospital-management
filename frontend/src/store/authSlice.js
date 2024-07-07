a06fb7499bcfe6c6573a873ec1eb2bf2f3aaeccb
sidharthproject <sidharthpatro001@gmail.com>
2024-07-07 10:15:44 +0530

    fourty one

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -1,9 +1,11 @@
+// authSlice.js
 import { createSlice } from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
 
 const initialState = {
   user: null,
   role: null,
-  token: null
+  token: Cookies.get("token") || null,
 };
 
 export const authSlice = createSlice({
@@ -13,20 +15,24 @@ export const authSlice = createSlice({
     loginStart: (state) => {
       state.user = null;
       state.role = null;
-      state.token =  Cookies.get("token") || null,
+      state.token = null;
     },
     loginSuccess: (state, action) => {
       const { user, token, role } = action.payload;
       state.user = user;
       state.token = token;
       state.role = role;
+      // Set cookie
+      Cookies.set("token", token, { expires: 7, secure: true, sameSite: "Strict" });
     },
     logout: (state) => {
       state.user = null;
       state.role = null;
       state.token = null;
-    }
-  }
+      // Clear cookie
+      Cookies.remove("token");
+    },
+  },
 });
 
 // Export actions
