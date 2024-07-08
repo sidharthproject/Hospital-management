8d8c2be45b1f815f6914d3e47dae64f2a5aeea53
sidharthproject <sidharthpatro001@gmail.com>
2024-07-08 09:56:38 +0530

    sixty seven

--- frontend/src/utils/uploadCloudinary.js
+++ frontend/src/utils/uploadCloudinary.js
@@ -1,27 +1,20 @@
 const upload_preset = import.meta.env.VITE_PRESET_NAME;
 const cloud_name = import.meta.env.VITE_CLOUD_NAME;
+import axios from 'axios';
 
 const uploadImageToCloudinary = async (file) => {
-  const uploadData = new FormData();
-  uploadData.append('file', file);
-  uploadData.append('upload_preset', upload_preset); // Corrected
-  uploadData.append('cloud_name', cloud_name);
+  const url = `https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/image/upload`;
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', upload_preset);
+  formData.append('cloud_name', cloud_name);
 
   try {
-    const res = await fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
-      method: 'POST',
-      body: uploadData,
-    });
-
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-
-    const data = await res.json();
-    return data;
+    const response = await axios.post(url, formData);
+    return response.data;
   } catch (error) {
-    console.error("Error uploading image to Cloudinary:", error);
-    return null;
+    console.error('Error uploading image to Cloudinary:', error);
+    throw error;
   }
 };
 
